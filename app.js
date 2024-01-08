c7b7bc0e4220fdc2c8cf1cce664801ff71930642
HP <devstipan.ecim@gmail.com>
2024-01-08 17:42:39 +0100

    Changes day 2

--- app.js
+++ app.js
@@ -1,10 +1,52 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
 
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header(
+        'Access-Control-Allow-Headers',
+        'Origin, X-Requested-With, Content-Type, Accept, Authorization'
+    );
+    if(req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, DELETE, PATCH, GET');
+        return res.status(200).json({});
+    }
+    next();
+
+});
+
+// routes that handle requests
 app.use('/products', productRoutes);
 app.use('/order', orderRoutes);
+
+// Error handling if we dont specifly existing rout we will get an error
+// we crate new object Error and by assigning it with new keywoard to error variable
+// then we give it a status 404 - which means not found 
+// then error gets passed with function called next that initiates next function in row
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error); // error gets passed to a next function 
+});
+
+// now it sets request status as error status
+// if error is not assigned we set error 500 witch means(intarnal server error)
+app.use((error, req, res, next) => {
+   res.status(error.status || 500);
+   res.json({
+    error: {
+        message: error.message
+    }
+   })
+});
 module.exports = app;
 
